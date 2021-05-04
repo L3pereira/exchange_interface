0b39234136dd3b625f298f71396cdb2e776278e3
L3pereira <75404065+L3pereira@users.noreply.github.com>
2021-05-04 14:26:41 +0100

    command line ui

--- orderbook/client_orderbook/src/main.rs
+++ orderbook/client_orderbook/src/main.rs
@@ -4,21 +4,30 @@ use tokio::{
     time::{sleep, Duration}
 };
 use tokio_stream::wrappers::ReceiverStream;
-
-
 use tonic::{transport::Server, Request, Response, Status};
-// use orderbook::orderbook_aggregator_client::{OrderbookAggregatorClient};
 use orderbook::{Summary, Level ,Empty};
 use orderbook::orderbook_aggregator_client::{OrderbookAggregatorClient};
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+
+#[tokio::main] 
+async fn main() ->Result<(), Box<dyn std::error::Error>>{
+
     let mut client = OrderbookAggregatorClient::connect("http://[::1]:50051").await?;
 
     // let response = client.book_summary(Request::new(Empty)).await?;
     let mut stream = client.book_summary(Request::new(Empty{})).await?.into_inner();
-
+   
     while let Some(data) = stream.message().await? {
-        println!("NOTE = {:?}", data);
+        println!("\n\nPrice|Amount|Exchange");
+        for ask in data.asks.iter(){
+            println!("{:?}|{:?}|{:?}", ask.price, ask.amount, ask.exchange);
+        }
+        println!("\nSpread---------{:?}-------------\n", data.spread);
+        for bid in data.bids.iter().rev(){
+            println!("{:?}|{:?}|{:?}", bid.price, bid.amount, bid.exchange);
+        }
+        println!("\n----------------------------------------------------\n");
     }
     Ok(())
 }
